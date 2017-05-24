779a961983ce7b364a3f070a28618c3eed837e93
ronniec95 <ronniec95@lineone.net>
2017-05-24 17:11:20 +0100

    Initial version - testing

---
+++ resource.h
@@ -0,0 +1,47 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LWWin32.rc
+//
+#define IDR_MENU1                       101
+#define IDR_MENU2                       102
+#define IDR_STATUSBAR1                  104
+#define IDR_MENU3                       105
+#define IDS_TOOLBAR                     108
+#define IDB_BITMAP1                     109
+#define IDM_FILENEW                     109
+#define TB_CMD_1                        110
+#define TB_CMD_2                        111
+#define IDR_MENU4                       111
+#define IDB_BITMAP2                     112
+#define IDM_FILEEXIT                    112
+#define IDM_EDITCUT                     113
+#define IDM_EDITCOPY                    114
+#define IDM_EDITPASTE                   115
+#define IDM_EDITPASTEFOO                116
+#define IDM_EDITPASTEFOO1               117
+#define IDM_EDITPASTEFOO2               118
+#define ID_FILE_EXIT                    40001
+#define ID_EDIT_CUT                     40002
+#define ID_EDIT_COPY                    40003
+#define ID_EDIT_PASTE                   40004
+#define ID_EDIT_PREFERENCES             40005
+#define ID_SHOWMESSAGE_EXIT             40006
+#define ID_FILE_OPEN                    40008
+#define ID_FILE_SAVE                    40010
+#define ID_FILE_NEW                     40011
+#define ID_FOO                          40012
+#define ID_FOO_BAR                      40013
+#define ID_FOO_IT                       40014
+#define ID_TPN_TPN                      40015
+#define ID_TPN_TPN40016                 40016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40017
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
